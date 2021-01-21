0ad679e54bfcf8bdcc11393e519626de2fd390af
mateuswetah <mateus.m.luna@gmail.com>
2021-01-21 16:16:06 -0300

    New description to most view modes

--- tainacan-extra-viewmodes/tainacan-extra-viewmodes.php
+++ tainacan-extra-viewmodes/tainacan-extra-viewmodes.php
@@ -23,7 +23,7 @@ function tainacan_extra_viewmodes_register_templates() {
 		// Registering the view modes
 		tainacan_register_view_mode('mosaic', [
 			'label' 			=> 'Mosaic',
-			'description' 		=> __('A mosaic view', 'tainacan-extra-viewmodes'),
+			'description' 		=> __('A simple and marginless mosaic of item thumbnails.', 'tainacan-extra-viewmodes'),
 			'icon' 				=> '<span class="icon"><i><svg fill="var(--tainacan-info-color, #555758)" xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="-2 -2 48 48"><path d="M24.981 7.464H4.898c-.758 0-1.372.615-1.372 1.373v14.816c0 .758.615 1.373 1.372 1.373h20.084c.758 0 1.373-.615 1.373-1.373V8.838c0-.76-.615-1.374-1.374-1.374zM29.183 15.527h7.927c.758 0 1.373-.615 1.373-1.374h-.001V8.838c0-.76-.614-1.373-1.373-1.373h-7.926c-.757 0-1.372.614-1.372 1.373v5.315c0 .76.615 1.374 1.372 1.374zM38.482 18.336c0-.758-.614-1.373-1.373-1.373h-7.926c-.757 0-1.372.615-1.372 1.373v5.317c0 .758.615 1.373 1.372 1.373h7.927c.758 0 1.373-.615 1.373-1.373h-.001zM38.481 27.847c0-.759-.613-1.373-1.373-1.373h-7.925c-.757 0-1.373.614-1.373 1.373v5.316c0 .758.616 1.373 1.373 1.373h7.927c.758 0 1.373-.615 1.373-1.373h-.002zM24.962 26.473h-7.926c-.758 0-1.373.615-1.373 1.373v5.316c0 .759.615 1.373 1.373 1.373h7.926c.759 0 1.373-.614 1.373-1.373v-5.315c0-.759-.613-1.374-1.373-1.374zM12.815 26.473H4.89c-.758 0-1.373.615-1.373 1.373v5.316c0 .759.615 1.373 1.373 1.373h7.925c.76 0 1.374-.614 1.374-1.373v-5.315c0-.759-.615-1.374-1.374-1.374z"/></svg></i></span>',
 			'dynamic_metadata' 	=> false,
 			'template' 			=> __DIR__ . '/templates/view-mode-mosaic.php',
@@ -31,42 +31,42 @@ function tainacan_extra_viewmodes_register_templates() {
 		]);
 		tainacan_register_view_mode('frame', [
 			'label' 			=> 'Frame',
-			'description' 		=> __('A frame view, made for gallery expositions', 'tainacan-extra-viewmodes'),
+			'description' 		=> __('A centered aligned, framed thumbnails view, like gallery expositions.', 'tainacan-extra-viewmodes'),
 			'icon' 				=> '<span class="icon"><i><svg fill="var(--tainacan-info-color, #555758)" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M4.188 21.65h15.624a.103.103 0 00.103-.104V2.454a.103.103 0 00-.103-.103H4.188a.103.103 0 00-.103.103v19.092c0 .057.046.103.103.103zm1.394-1.518V3.848h12.837v16.284z"/><path d="M15.921 6.242H8.079a.103.103 0 00-.103.103v11.29c0 .057.046.104.103.104h7.842a.103.103 0 00.104-.104V6.345a.103.103 0 00-.104-.103z"/></svg></i></span>',
 			'dynamic_metadata' 	=> false,
 			'template' 			=> __DIR__ . '/templates/view-mode-frame.php',
 		]);
 		tainacan_register_view_mode('exhibition', [
 			'label' 			=> 'Exhibition',
-			'description' 		=> __('A framed view with metadata, made for gallery expositions', 'tainacan-extra-viewmodes'),
+			'description' 		=> __('A framed record view, where image and metadata are expanded on hover.', 'tainacan-extra-viewmodes'),
 			'icon' 				=> '<span class="icon"><i><svg fill="var(--tainacan-info-color, #555758)" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M5.761 5.138h12.42v7.43H5.762z"/><path d="M20.28 2.869H3.72v16.787h-.794v1.475h2.892v-1.475h-.794v-4.82h4.14v.85h-.567v1.815h6.806v-1.815h-.567v-.85h4.14v4.82h-.794v1.475h2.892v-1.475h-.794zM14.27 15.686H9.73v-.85h4.538zm4.99-1.985H4.741V4.003h14.462v9.698z"/></svg></i></span>',
 			'dynamic_metadata' 	=> true,
 			'template' 			=> __DIR__ . '/templates/view-mode-exhibition.php',
 		]);
 		tainacan_register_view_mode('books', [
 			'label' 			=> 'Books',
-			'description' 		=> __('A books view, made for library visualizations', 'tainacan-extra-viewmodes'),
+			'description' 		=> __('A book cover view, made for library visualizations.', 'tainacan-extra-viewmodes'),
 			'icon' 				=> '<span class="icon"><i><svg fill="var(--tainacan-info-color, #555758)" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M16.062 4.569l-7.1-1.244c-.073-.019-.146-.019-.219-.019a1.3 1.3 0 00-.933.385L6.163 5.337a.914.914 0 00-.256.64v12.187c0 .513.366.952.878 1.043l8.454 1.482a.409.409 0 00.457-.402V8.357a1.2 1.2 0 00-1.006-1.19L7.9 5.96a.492.492 0 01-.256-.842l.805-.805a.432.432 0 01.293-.128h.073l7.1 1.244c.75.128 1.28.787 1.28 1.537v11.692c0 .22.257.311.403.165l.11-.11c.238-.238.384-.585.384-.933V6.984a2.486 2.486 0 00-2.03-2.415z"/></svg></i></span>',
 			'dynamic_metadata' 	=> false,
 			'template' 			=> __DIR__ . '/templates/view-mode-books.php',
 		]);
 		tainacan_register_view_mode('polaroid', [
 			'label' 			=> 'Polaroid',
-			'description' 		=> __('A framed picture view, similar to polaroid photographs', 'tainacan-extra-viewmodes'),
+			'description' 		=> __('A framed picture view, similar to polaroid photographs.', 'tainacan-extra-viewmodes'),
 			'icon' 				=> '<span class="icon"><i><svg fill="var(--tainacan-info-color, #555758)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23.362 24" width="24" height="24"><path d="M1.657 22.298h13.769V5.692H1.657zm.942-15.47h11.885v11.664H2.599z"/><path d="M8.02 1.702L7.627 5.25h.948l.257-2.314 11.813 1.313-1.29 11.593-3.531-.393v3.83l4.047.45 1.835-16.504z"/></svg></i></span>',
 			'dynamic_metadata' 	=> true,
 			'template' 			=> __DIR__ . '/templates/view-mode-polaroid.php',
 		]);
 		tainacan_register_view_mode('document', [
 			'label' 			=> 'Document',
-			'description' 		=> __('A document view, for displaying papers and published research', 'tainacan-extra-viewmodes'),
+			'description' 		=> __('Records with stacked papers style, for displaying published research.', 'tainacan-extra-viewmodes'),
 			'icon' 				=> '<span class="icon"><i><svg fill="var(--tainacan-info-color, #555758)" xmlns="http://www.w3.org/2000/svg" height="24" width="24" version="1.0"><path d="M5.148 17.717l-1.554-.462 1.858-6.176zM18.92 5.638l.784.233-.938 3.118zM8.156 1.938l4.51 1.34-4.855-.192zM12.736 21.243l2.335.092-.181.603-2.269-.674-4.34.798L5.339 5.25 17.463 3.02l2.943 16.813zM6.534 4.575l.05-1.08 4.796.19zM7.44 20.021l-1.94-.076.4-8.719z"/><path d="M18.09 21.183l-2.296-.09 2.32-.427zM18.588 4.373l-.104 2.27-.4-2.29z"/></svg></i></span>',
 			'dynamic_metadata' 	=> true,
 			'template' 			=> __DIR__ . '/templates/view-mode-document.php',
 		]);
 		tainacan_register_view_mode('albums', [
 			'label' 			=> 'Albums',
-			'description' 		=> __('A musical albums view', 'tainacan-extra-viewmodes'),
+			'description' 		=> __('Thumbnails displayed as album covers with a disk inside.', 'tainacan-extra-viewmodes'),
 			'icon' 				=> '<span class="icon"><i><svg fill="var(--tainacan-info-color, #555758)" xmlns="http://www.w3.org/2000/svg" height="24" width="24" data-name="Layer 2"><path d="M2.745 3.963a1.02 1.02 0 00-1.02 1.02v14.034a1.02 1.02 0 001.02 1.02h12.413a1.02 1.02 0 001.02-1.02V4.983a1.02 1.02 0 00-1.02-1.02zm9.58 2.886l.035 7.623h-.006c-.028.695-.445 1.024-1.063 1.23-.751.248-1.512.096-1.695-.464-.183-.56.286-1.229 1.027-1.478a1.855 1.855 0 01.623-.1v-2.643L7.313 12.36v3.144s0 .023.014.037c.185.572-.286 1.212-1.026 1.46-.74.25-1.512 0-1.695-.571-.183-.572.286-1.223 1.026-1.472a1.895 1.895 0 01.571-.111V8.9zM22.275 11.97a7.135 7.135 0 01-4.688 6.665v-4.656a2.032 2.032 0 000-3.993V5.32a7.1 7.1 0 014.673 6.65z"/><path d="M10.937 8.899v1.2l-3.45 1.18v-1.23z"/></svg></i></span>',
 			'dynamic_metadata' 	=> false,
 			'template' 			=> __DIR__ . '/templates/view-mode-albums.php',
